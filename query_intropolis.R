952ceb362f21f2dc302f3424224b85052e5d005e
lcolladotor <lcollado@jhsph.edu>
2016-09-13 11:59:43 -0400

    Add a bit more info

--- query_intropolis.R
+++ query_intropolis.R
@@ -8,6 +8,7 @@ message(paste(Sys.time(), 'loading rse_jx.Rdata'))
 system.time( load('rse_jx.Rdata') )
 
 ## Get junctions in the data
+message(paste(Sys.time(), 'creating jx.tsv file'))
 jx <- rowRanges(rse_jx)
 jx_df <- data.frame(chr = seqnames(jx), start = start(jx), end = end(jx), strand = strand(jx))
 
